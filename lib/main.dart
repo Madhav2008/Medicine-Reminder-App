deb56accc902796d23a8d452212b1f883ec1fea8
arora <aroramadhav65@gmail.com>
2022-05-13 20:35:24 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
