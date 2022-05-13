cffb254b83665b7d1fd8f02549bb7d7a4b9d020d
arora <aroramadhav65@gmail.com>
2022-05-13 20:35:30 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
