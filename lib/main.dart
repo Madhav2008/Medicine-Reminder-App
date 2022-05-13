06a7f6cbfc3579825b125e02dc8e0c67f119924e
arora <aroramadhav65@gmail.com>
2022-05-13 20:41:49 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
