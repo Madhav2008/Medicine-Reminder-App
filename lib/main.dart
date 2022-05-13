6f830962a6dec084f3fc0dc6dcb3176abe64098f
arora <aroramadhav65@gmail.com>
2022-05-13 20:35:53 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
 
