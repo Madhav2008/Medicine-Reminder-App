87ed61d18ec0e2a9da71de7b2b8333d47ea470d0
arora <aroramadhav65@gmail.com>
2022-05-13 20:39:26 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
