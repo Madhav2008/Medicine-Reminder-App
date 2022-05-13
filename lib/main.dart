6fb870fc7ba92c9a390d468d54b37143772bc387
arora <aroramadhav65@gmail.com>
2022-05-13 20:37:53 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
