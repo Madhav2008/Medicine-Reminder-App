da6bfae52f020868a879155ed2efad54992333bf
arora <aroramadhav65@gmail.com>
2022-05-13 20:39:46 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
