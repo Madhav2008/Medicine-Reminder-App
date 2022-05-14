13091f54238f21c25d15c8fa44da1e27c5c88489
arora <aroramadhav65@gmail.com>
2022-05-14 13:38:04 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -148,7 +148,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       )
                     : 
-                    
+                    Text('')
                     // MedicinesList(
                     //     dailyPills,
                     //     setData,
