d1f826c98a11681fcf352266f2bb48caed8002c3
arora <aroramadhav65@gmail.com>
2022-05-14 12:57:21 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Medicine Reminder',
+      title: 'Medicine Reminder App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
