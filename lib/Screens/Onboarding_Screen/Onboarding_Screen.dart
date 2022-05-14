9ce747b70f429986ce8897079a8997e06977c442
arora <aroramadhav65@gmail.com>
2022-05-14 13:11:08 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -40,6 +40,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                       style: TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
+                        height:1.3,
                       ),
                       textAlign: TextAlign.center,
                       maxLines: 2,
