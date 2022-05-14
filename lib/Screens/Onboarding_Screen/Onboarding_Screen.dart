2a1038a9779ac59873c65e58dd193da4aad73bff
arora <aroramadhav65@gmail.com>
2022-05-14 12:42:17 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -18,7 +18,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           children: [
             SizedBox(
               height: deviceHeight * 0.04,
-            ),Image.asset('assets/images/welcome_image.png',
+            ),
+            Image.asset('assets/images/welcome_image.png',
                 width: double.infinity, height: deviceHeight * 0.4),
           ],
         ),
