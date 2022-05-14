233c30343866f9c25eae7b4107c7518e95e45565
arora <aroramadhav65@gmail.com>
2022-05-14 13:54:24 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class AddMedicineScreen extends StatefulWidget {
+  const AddMedicineScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<AddMedicineScreen> createState() => _AddMedicineScreenState();
+}
+
+class _AddMedicineScreenState extends State<AddMedicineScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
