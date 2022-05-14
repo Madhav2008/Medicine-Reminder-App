a05d62672e2eb62b5b42244acb2ec16d43a934c9
arora <aroramadhav65@gmail.com>
2022-05-14 13:58:37 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -59,7 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final Widget addButton = FloatingActionButton(
       elevation: 2.0,
       onPressed: () async {
-        Navigator.push(context, MaterialPageRoute(builder: builder))
+        Navigator.push(context, MaterialPageRoute(builder: (builder)))
         // await Navigator.pushNamed(context, "/add_new_medicine")
         //     .then((_) => setData());
       },
