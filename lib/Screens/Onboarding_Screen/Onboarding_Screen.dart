676d025266a198b0ac239cae4b075b5fa7250e9e
arora <aroramadhav65@gmail.com>
2022-05-14 12:43:24 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -26,7 +26,40 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
             SizedBox(
               height: deviceHeight * 0.05,
+            ),Column(
+      children: [
+        Container(
+          height: deviceHeight * 0.15,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: AutoSizeText(
+                "Be in control of your meds",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline1
+                    .copyWith(color: Colors.black, height: 1.3),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+              ),
             ),
+          ),
+        Container(
+          height: deviceHeight * 0.15,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+            child: AutoSizeText(
+              "An easy-to-use and reliable app that helps you remember to take your meds at the right time",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(color: Colors.grey[600], height: 1.3,),
+              textAlign: TextAlign.center,
+              maxLines: 3,
+            ),
+          ),
+        ),
+      ],
+    );
           ],
         ),
       ),
