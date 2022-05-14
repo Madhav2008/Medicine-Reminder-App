a5c13f5d84fe38b414608d73e1c1548f0d9ac9ba
arora <aroramadhav65@gmail.com>
2022-05-14 13:57:07 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -15,7 +15,8 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
         title: Text(
           'Add Medicine',
           style: TextStyle(
-            fontSize: 25,color: Colors.black,
+            fontSize: 25,
+            color: Colors.black,
           ),
         ),
       ),
