fb8423b6a1bb1bfcb12afeac651b9a9fd5078095
arora <aroramadhav65@gmail.com>
2022-05-14 13:55:21 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -11,7 +11,7 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar()
     );
   }
 }
