d04ae8e380ca95115acaf385e50a07a0ed00bfec
arora <aroramadhav65@gmail.com>
2022-05-14 13:06:33 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Medicine Reminder',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
