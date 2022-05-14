56681a76ba3e22bb3706c36a07b0a7197495d7de
arora <aroramadhav65@gmail.com>
2022-05-14 13:24:55 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Medicine Reminder',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: OnboardingScreen(),
     );
