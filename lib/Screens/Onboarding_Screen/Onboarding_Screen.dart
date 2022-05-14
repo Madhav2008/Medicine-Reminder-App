df185cf46cebeb829f8d645d1bed6ccd80879dc4
arora <aroramadhav65@gmail.com>
2022-05-14 12:39:06 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
