37bc753f4e743db1e133e6ccf827c66a179c9fad
arora <aroramadhav65@gmail.com>
2022-05-14 12:41:33 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -10,6 +10,7 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
     return Scaffold(
       body: SafeArea(
         child: Column(
