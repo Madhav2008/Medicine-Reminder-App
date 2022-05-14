47798ffa648a941a2249e90d11abe9e4111947a8
arora <aroramadhav65@gmail.com>
2022-05-14 12:56:42 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medicine_reminder_app/Screens/Onboarding_Screen/Onboarding_Screen.dart';
 
 void main() {
   runApp(
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: OnboardingScreen()
     );
   }
 }
