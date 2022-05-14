6282587b1d2c3b80269a5461ff47faf91121556e
arora <aroramadhav65@gmail.com>
2022-05-14 13:36:16 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -1,5 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:animated_widgets/animated_text_kit.dart';
+import 'package:animated_widgets/animated_widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
