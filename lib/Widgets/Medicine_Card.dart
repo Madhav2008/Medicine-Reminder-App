8e3acb65a3c68dca161cee1ed2da0343bced4206
arora <aroramadhav65@gmail.com>
2022-05-15 20:08:37 +0530

    Update

--- lib/Widgets/Medicine_Card.dart
+++ lib/Widgets/Medicine_Card.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MedicineCard extends StatefulWidget {
-  const MedicineCard({Key? key, required this.avatar, required this.name, required this.info, required this.time,}) : super(key: key);
+  const MedicineCard({
+    Key? key,
+    required this.avatar,
+    required this.name,
+    required this.info,
+    required this.time,
+  }) : super(key: key);
 
   @override
   State<MedicineCard> createState() => _MedicineCardState();
