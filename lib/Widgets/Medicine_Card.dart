fa0ceb10183251be7e890819c3b28fb7b5e2f181
arora <aroramadhav65@gmail.com>
2022-05-15 20:09:35 +0530

    Update

--- lib/Widgets/Medicine_Card.dart
+++ lib/Widgets/Medicine_Card.dart
@@ -10,7 +10,7 @@ class MedicineCard extends StatefulWidget {
   }) : super(key: key);
 
   final String avatar;
-  final String avatar;
+  final String name;
   final String avatar;
   final String avatar;
 
