a2012d9df21ab8de85e6544ec495cc4592efa530
arora <aroramadhav65@gmail.com>
2022-05-15 19:32:58 +0530

    Update

--- lib/Widgets/Slider.dart
+++ lib/Widgets/Slider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class UserSlider extends StatelessWidget {
   final Function handler;
   final int howManyWeeks;
-  UserSlider(this.handler,this.howManyWeeks);
+  UserSlider(this.handler, this.howManyWeeks);
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +11,13 @@ class UserSlider extends StatelessWidget {
       children: [
         Expanded(
             child: PlatformSlider(
-              divisions: 11,
-              min: 1,
-              max: 10,
-              value: howManyWeeks,
-              color: Theme.of(context).primaryColor,
-              handler:  this.handler,)),
+          divisions: 11,
+          min: 1,
+          max: 10,
+          value: howManyWeeks,
+          color: Theme.of(context).primaryColor,
+          handler: this.handler,
+        )),
       ],
     );
   }
