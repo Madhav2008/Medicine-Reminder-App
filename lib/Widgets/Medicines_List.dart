75a9c91ba683a234b3bd6f959cb00d857d08c5dd
arora <aroramadhav65@gmail.com>
2022-05-15 19:41:36 +0530

    Update

--- lib/Widgets/Medicines_List.dart
+++ lib/Widgets/Medicines_List.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:medicine/models/pill.dart';
-import '../../screens/home/medicine_card.dart';
 
 class MedicinesList extends StatelessWidget {
   final List<Pill> listOfMedicines;
