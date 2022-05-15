273078a414bae53b4507291dea0d6e4114938d7b
arora <aroramadhav65@gmail.com>
2022-05-15 20:06:58 +0530

    Update

--- lib/Widgets/Medicine_Card.dart
+++ lib/Widgets/Medicine_Card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MedicineCard extends StatefulWidget {
-  const MedicineCard({ Key? key }) : super(key: key);
+  const MedicineCard({Key? key}) : super(key: key);
 
   @override
   State<MedicineCard> createState() => _MedicineCardState();
@@ -10,8 +10,6 @@ class MedicineCard extends StatefulWidget {
 class _MedicineCardState extends State<MedicineCard> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-    
-    );
+    return ListTile();
   }
 }
