b479dafd193cc98427d6c42f8ca2d314cc82ec44
arora <aroramadhav65@gmail.com>
2022-05-15 19:54:29 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -26,6 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
     year: 2022,
     month: 5,
   );
+  isLoading = true;
   Future setData() async {
     // allListOfPills.clear();
     // (await _repository.getAllData("Pills")).forEach((pillMap) {
