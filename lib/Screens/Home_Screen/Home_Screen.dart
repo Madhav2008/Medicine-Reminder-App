774dbcffa0528fbfb688d409325fa0619b36563b
arora <aroramadhav65@gmail.com>
2022-05-15 16:13:17 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -96,7 +96,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: EdgeInsets.symmetric(horizontal: 5.0),
                 ),
                 SizedBox(height: deviceHeight * 0.03),
-                Future.delayed(duration)
+                Future.delayed(
+      Duration(
+        seconds: 1,
+      ),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LandingPage(),
+          ),
+        );
+      },
+    );
                 SizedBox(
                   width: double.infinity,
                   // height: 100,
