c0e67e443ecf2ec359cca6c40ca99db0703cc81d
arora <aroramadhav65@gmail.com>
2022-05-15 20:17:30 +0530

    Update

--- lib/Widgets/Medicine_Card.dart
+++ lib/Widgets/Medicine_Card.dart
@@ -45,6 +45,7 @@ class _MedicineCardState extends State<MedicineCard> {
         widget.info,
         style: TextStyle(
           color: Colors.grey,
+          fontSize: 17,
         ),
       ),
     );
