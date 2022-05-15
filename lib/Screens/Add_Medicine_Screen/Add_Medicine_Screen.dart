870e96a7b07162f292849ec127f620f4e785c543
arora <aroramadhav65@gmail.com>
2022-05-15 19:37:31 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -1,31 +1,31 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class AddMedicineScreen extends StatefulWidget {
-  const AddMedicineScreen({Key? key}) : super(key: key);
+// class AddMedicineScreen extends StatefulWidget {
+//   const AddMedicineScreen({Key? key}) : super(key: key);
 
-  @override
-  State<AddMedicineScreen> createState() => _AddMedicineScreenState();
-}
+//   @override
+//   State<AddMedicineScreen> createState() => _AddMedicineScreenState();
+// }
 
-class _AddMedicineScreenState extends State<AddMedicineScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Add Medicine',
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
-}
+// class _AddMedicineScreenState extends State<AddMedicineScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Add Medicine',
+//           style: TextStyle(
+//             fontSize: 25,
+//             color: Colors.black,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         backgroundColor: Colors.transparent,
+//         elevation: 0,
+//         iconTheme: IconThemeData(
+//           color: Colors.black,
+//         ),
+//       ),
+//     );
+//   }
+// }
