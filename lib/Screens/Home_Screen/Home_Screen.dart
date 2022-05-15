d212759385092f562bbb373c1f59c7e29f4512d1
arora <aroramadhav65@gmail.com>
2022-05-15 23:07:23 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -233,7 +233,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: CircleAvatar(
                   foregroundColor: Theme.of(context).primaryColor,
                   backgroundColor: Colors.grey,
-                  backgroundImage: NetworkImage(medi[i].avatarUrl),
+                  backgroundImage: NetworkImage(medi[i].avatar),
                 ),
               ),
               title: Row(
