fd1b0c59deb35bfc026d4acc4f7f83eac855bbe8
arora <aroramadhav65@gmail.com>
2022-05-15 16:16:55 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   startTime() async {
     var _duration = new Duration(seconds: 2);
-    return new Timer(_duration, (() => );
+    return new Timer(_duration, (() => Lottie.network('url'));
   }
 
   @override
