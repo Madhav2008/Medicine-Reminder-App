0e357d964d0a7fa27182a4aca3e225ad00450093
arora <aroramadhav65@gmail.com>
2022-05-15 19:45:52 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -171,7 +171,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   },
                 ),
                 MedicinesList(
-                    dailyPills, setData, flutterLocalNotificationsPlugin,)
+                  dailyPills,
+                  setData,
+                  flutterLocalNotificationsPlugin,
+                )
               ],
             ),
           ),
