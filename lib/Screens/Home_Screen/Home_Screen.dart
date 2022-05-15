d3c7580f68fe8946135dac6e69947facda7fb6b3
arora <aroramadhav65@gmail.com>
2022-05-15 20:41:46 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -192,7 +192,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         },
                       )
                     : ListView.builder(
-                        itemCount: medi.length,
+                        itemCount: dummyData.length,
                         itemBuilder: (context, i) {
                           return MedicineCard(
                             avatar: '',
