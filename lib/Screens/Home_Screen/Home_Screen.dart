9a8c7c73e5736215239cccee8db2fdfc9a6b456f
arora <aroramadhav65@gmail.com>
2022-05-15 16:18:55 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -112,29 +112,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 SizedBox(height: deviceHeight * 0.03),
                 FutureBuilder(builder: (context, index)=>),
-                SizedBox(
-                  width: double.infinity,
-                  // height: 100,
-                  child: Column(
-                    children: [
-                      Lottie.network(
-                        'https://assets4.lottiefiles.com/packages/lf20_acxgzi0c.json',
-                        width: 200,
-                        height: 200,
-                      ),
-                      WavyAnimatedTextKit(
-                        textStyle: TextStyle(
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                        text: ["Loading..."],
-                        isRepeatingAnimation: true,
-                        speed: Duration(milliseconds: 150),
-                      ),
-                    ],
-                  ),
-                ),
               ],
             ),
           ),
