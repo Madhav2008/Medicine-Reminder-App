88b7732f13ff06c1a5a1fc8718d43bdbcb70129b
arora <aroramadhav65@gmail.com>
2022-05-15 16:07:40 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -96,6 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: EdgeInsets.symmetric(horizontal: 5.0),
                 ),
                 SizedBox(height: deviceHeight * 0.03),
+                Future.delayed(duration)
                 SizedBox(
                   width: double.infinity,
                   // height: 100,
