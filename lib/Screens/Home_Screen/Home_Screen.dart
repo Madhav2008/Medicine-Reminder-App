5e381c85408cfc6c2abd0ca35716ee4ff5be4361
arora <aroramadhav65@gmail.com>
2022-05-15 19:45:31 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _lastChooseDay = 0;
-  final dailyPills;
+  final dailyPills = '';
   final CalendarDayModel _days = CalendarDayModel(
     dayLetter: 'Sunday',
     dayNumber: 15,
