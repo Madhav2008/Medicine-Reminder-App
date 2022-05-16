50cdd9a8a53394df9288e911ae22374026ea4fa9
arora <aroramadhav65@gmail.com>
2022-05-16 16:04:35 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -80,7 +80,7 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
                   ),
                 ),
                 Container(
-                  width: MediaQuery.of(context).size.width - 320,
+                  width: MediaQuery.of(context).size.width - 310,
                   child: TextField(
                     decoration: InputDecoration(
                       labelText: 'Medicine Name',
