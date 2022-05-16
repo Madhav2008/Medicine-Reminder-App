8bed5b0efec117a96c0fb15ee4311d02bb433e65
arora <aroramadhav65@gmail.com>
2022-05-16 12:13:16 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -156,6 +156,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   height: deviceHeight * 0.03,
                 ),
+                ListView.builder(
+                        itemCount: medi.length,
+                        itemBuilder: (context, i) {
+                          return MedicineCard(
+                            avatar: medi[i].avatar,
+                            info: medi[i].info,
+                            name: medi[i].name,
+                            time: medi[i].time,
+                          );
+                        },
+                      ),
                 // isLoading
                 //     ? isEmpty
                 //         ? Lottie.network(
