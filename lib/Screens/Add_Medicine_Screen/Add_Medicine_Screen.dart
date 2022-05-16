b6a161fb23ae9c087c17e49f4dd18052e8a82c76
arora <aroramadhav65@gmail.com>
2022-05-16 18:53:32 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -46,7 +46,8 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+           padding: const EdgeInsets.only(
+              left: 20.0, right: 20.0, top: 30.0, bottom: 30.0),
           child: Column(
             children: [
               Container(
