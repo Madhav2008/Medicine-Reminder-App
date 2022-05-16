5ff3a73b810940dbc0fe9d3edb6d3709d9c9a7f7
arora <aroramadhav65@gmail.com>
2022-05-16 12:15:06 +0530

    Update

--- lib/Widgets/Medicines.dart
+++ lib/Widgets/Medicines.dart
@@ -5,8 +5,16 @@ class Medicines extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return ListView.builder(
+                        itemCount: medi.length,
+                        itemBuilder: (context, i) {
+                          return MedicineCard(
+                            avatar: medi[i].avatar,
+                            info: medi[i].info,
+                            name: medi[i].name,
+                            time: medi[i].time,
+                          );
+                        },
+                      ),
   }
 }
