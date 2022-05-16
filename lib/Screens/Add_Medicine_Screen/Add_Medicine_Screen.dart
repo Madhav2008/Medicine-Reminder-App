e5060cf529ba454aecf7486a7d48545f5d850a65
arora <aroramadhav65@gmail.com>
2022-05-16 16:31:40 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -92,7 +92,7 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
                 Container(
                   width: MediaQuery.of(context).size.width - 305,
                   decoration: BoxDecoration(
-                    border: Border.all(width: 1),
+                    border: Border.all(width: 1,color: Colors.grey.shade700),
                   ),
                   child: DropdownButton<String>(
                     items: _category.map((String dropDownStringItem) {
