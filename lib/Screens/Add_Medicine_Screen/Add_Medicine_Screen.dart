686f4da9faddc2ad432a9065e718d2506d15c66b
arora <aroramadhav65@gmail.com>
2022-05-16 16:22:12 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -117,7 +117,6 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
                   ),
                 ),
               ),
-            ),
               ],
             ),
           ),
