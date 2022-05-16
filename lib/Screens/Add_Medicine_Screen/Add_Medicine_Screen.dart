3302818942de27f70281ba7b24983ad3ffcf90a7
arora <aroramadhav65@gmail.com>
2022-05-16 16:00:57 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -79,6 +79,28 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
                     ),
                   ),
                 ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Medicine Name',
+                      labelStyle: TextStyle(color: Colors.grey.shade700),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade700,
+                          width: 0.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade700,
+                          width: 0.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
