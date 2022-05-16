531ce4c32145ce621a99600056b45a9632108666
arora <aroramadhav65@gmail.com>
2022-05-16 13:30:08 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -31,7 +31,7 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
           TextField(
             decoration: InputDecoration(
               labelText: 'Medicine Name',
-              border: InputBorder.
+              border: InputBorder.all
             ),
           ),
         ],
