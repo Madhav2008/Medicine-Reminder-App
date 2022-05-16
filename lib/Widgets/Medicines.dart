c2fb6085b80cd23d19682bec87af58da9216cab9
arora <aroramadhav65@gmail.com>
2022-05-16 12:15:10 +0530

    Update

--- lib/Widgets/Medicines.dart
+++ lib/Widgets/Medicines.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Medicines extends StatelessWidget {
-  const Medicines({ Key? key }) : super(key: key);
+  const Medicines({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-                        itemCount: medi.length,
-                        itemBuilder: (context, i) {
-                          return MedicineCard(
-                            avatar: medi[i].avatar,
-                            info: medi[i].info,
-                            name: medi[i].name,
-                            time: medi[i].time,
-                          );
-                        },
-                      ),
+      itemCount: medi.length,
+      itemBuilder: (context, i) {
+        return MedicineCard(
+          avatar: medi[i].avatar,
+          info: medi[i].info,
+          name: medi[i].name,
+          time: medi[i].time,
+        );
+      },
+    );
   }
 }
