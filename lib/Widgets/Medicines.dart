209de82b1e048abfedf8fbfd52add61757a72ea1
arora <aroramadhav65@gmail.com>
2022-05-16 12:37:00 +0530

    Update

--- lib/Widgets/Medicines.dart
+++ lib/Widgets/Medicines.dart
@@ -12,7 +12,7 @@ class Medicines extends StatelessWidget {
     //   itemBuilder: (context, i) {
         return ListTile(
           tileColor: Colors.white,
-          leading: Image.asset(medi[context].avatar),
+          leading: Image.asset(medi[].avatar),
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
