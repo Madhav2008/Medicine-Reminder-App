3343455a9de31e28ebe24ae29e56ca0f82e1ef57
arora <aroramadhav65@gmail.com>
2022-05-16 12:51:20 +0530

    Update

--- lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
+++ lib/Screens/Onboarding_Screen/Onboarding_Screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:medicine_reminder_app/Screens/Home_Screen/Home_Screen.dart';
-import 'package:medicine_reminder_app/Widgets/Medicines.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -81,7 +80,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => Medicines(),
+                        builder: (context) => HomeScreen(),
                       ),
                     );
                   },
