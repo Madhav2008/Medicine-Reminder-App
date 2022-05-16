653df89fdbf39d4ac16f9d68222f5d1ac92ea9b9
arora <aroramadhav65@gmail.com>
2022-05-16 15:59:02 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -55,7 +55,9 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
           ),
           Container(
             width: MediaQuery.of(context).size.width - 100,
-            child: Row(),
+            child: Row(
+              children: [],
+            ),
           ),
         ],
       ),
