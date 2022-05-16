1342aae14b854812af9f842d5d5f3ccebce394f4
arora <aroramadhav65@gmail.com>
2022-05-16 14:06:14 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -102,6 +102,94 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
               ],
             ),
           ),
+          Container(
+                width: MediaQuery.of(context).size.width - 100,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.5,
+                      color: one,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            countryName,
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_drop_down,
+                        color: one,
+                        size: 25,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 90,
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          width: 1.5,
+                          color: one,
+                        ),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        countryCode,
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 180,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: one,
+                        ),
+                      ),
+                    ),
+                    child: TextField(
+                      controller: _phoneController,
+                      autofocus: true,
+                      cursorColor: one,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        counter: Offstage(),
+                        hintText: 'phone number',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
         ],
       ),
     );
