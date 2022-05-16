8ddbdca24a151d3ea3415ef2f2914bc46b1c5019
arora <aroramadhav65@gmail.com>
2022-05-16 19:16:45 +0530

    Update

--- lib/Screens/Home_Screen/Home_Screen.dart
+++ lib/Screens/Home_Screen/Home_Screen.dart
@@ -187,7 +187,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           )
                     : 
-                    Medicines(),
+                    // Medicines(),
               ],
             ),
           ),
