cc140ee31541329d6f820d2a40bf5a2fa6eaa565
arora <aroramadhav65@gmail.com>
2022-05-17 22:41:27 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -191,7 +191,7 @@ class _AddMedicineScreenState extends State<AddMedicineScreen> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              SizedBox(width: 10),
+                              const SizedBox(width: 10),
                               Text(
                                 DateFormat.Hm().format(this.setDate),
                                 style: TextStyle(
