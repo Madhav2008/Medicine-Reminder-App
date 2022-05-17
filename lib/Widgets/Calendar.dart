abb80c7b05941a0bcf7b2bc1d0be425d5abe677d
arora <aroramadhav65@gmail.com>
2022-05-17 22:54:58 +0530

    Update

--- lib/Widgets/Calendar.dart
+++ lib/Widgets/Calendar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors
+// ignore_for_file: file_names, use_key_in_widget_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:medicine_reminder_app/Models/Calendar_Day_Model.dart';
