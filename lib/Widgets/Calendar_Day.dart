392827a668cbb9cbf6a2f2ce6554477e65e6522b
arora <aroramadhav65@gmail.com>
2022-05-17 22:53:28 +0530

    Update

--- lib/Widgets/Calendar_Day.dart
+++ lib/Widgets/Calendar_Day.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors_in_immutables
+// ignore_for_file: file_names
 
 import "package:flutter/material.dart";
 import 'package:medicine_reminder_app/Models/Calendar_Day_Model.dart';
