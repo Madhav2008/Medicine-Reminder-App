aaaad9addc37738a7c07a1f859fe680d9d9ad0c9
arora <aroramadhav65@gmail.com>
2022-05-17 22:35:10 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Medico',
+      title: 'MEDICO',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color.fromRGBO(7, 190, 200, 1),
