96b5fb7e125e649fcb6ba00923005e37e7dab8d1
arora <aroramadhav65@gmail.com>
2022-05-17 22:44:02 +0530

    Update

--- lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
+++ lib/Screens/Add_Medicine_Screen/Add_Medicine_Screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, sized_box_for_whitespace, deprecated_member_use
+// ignore_for_file: file_names, sized_box_for_whitespace, deprecated_member_use, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
